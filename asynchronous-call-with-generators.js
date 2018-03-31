4e0cfd32f211dfe0f78eee4cd59dbb2e9161d059
MuhammadLahin <muhammad.lahin@gmail.com>
2018-04-01 00:12:39 +0600

    Update asynchronous-call-with-generators.js

--- asynchronous-call-with-generators.js
+++ asynchronous-call-with-generators.js
@@ -6,5 +6,5 @@ function* get_gen() {
 };
 
 let myGen = get_gen();
-console.log(myGen.next().value.then(resp => console.log(resp)));
-console.log(myGen.next().value.then(resp => console.log(resp)));
+console.log(myGen.next().value.then(resp => console.log(resp))); // this myGen.value() iterator will trigger the first item from the get_gen() generator
+console.log(myGen.next().value.then(resp => console.log(resp))); // this myGen.value() iterator will trigger the second item from the get_gen() generator
